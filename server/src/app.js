65783340aa3a42ec3f7ed6ea3f45e0c08e3916f6
JaydeepUniyal010 <jaydeepuniyal07@gmail.com>
2026-07-22 00:48:01 +0530

    Add first middleware to Express application

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,11 @@ import express from "express";
 
 const app = express();
 
+app.use((req,res,next)=>{
+    console.log("Middleware executed");
+    next();
+});
+
 app.get("/", (req,res)=>{
     res.send("Welcome to smartTask API");
 });
