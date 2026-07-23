199fbda9a298082742f01b42c1bd30f2a38c956f
JaydeepUniyal010 <jaydeepuniyal07@gmail.com>
2026-07-24 01:56:19 +0530

    Add Express Router strucutre

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,11 @@
 import express from "express";
-import { loginUser } from "../controllers/user.controllers";
+import { loginUser } from "../controllers/user.controllers.js";
+import {userRouter} from "../routes/user.routes.js"
 
 const app = express();
 app.use(express.json());
+
+app.use(userRouter);
 
 app.use((req,res,next)=>{
     console.log("Middleware executed");
@@ -17,7 +20,7 @@ app.get("/about", (req,res)=>{
     res.send("About SmartTask");
 });
 
-app.post("/login", loginUser);
+
 
 app.get("/random", (req,res)=>{
     res.send("Random Page for Task")
