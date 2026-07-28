10d9230a924c94bacc950e0b6eb4dbffe059fe2c
JaydeepUniyal010 <jaydeepuniyal07@gmail.com>
2026-07-29 00:00:53 +0530

    env update

--- server/src/server.js
+++ server/src/server.js
@@ -4,6 +4,8 @@ import dotenv from "dotenv";
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
+console.log(process.env.JWT_SECRET);
+
 app.listen(PORT, () =>{
 console.log(`Server is running on port ${PORT}`);
 });
