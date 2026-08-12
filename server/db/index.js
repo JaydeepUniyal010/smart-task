8b68f0ac291830526bfccf4a8e4a6dabae7bab2b
JaydeepUniyal010 <jaydeepuniyal07@gmail.com>
2026-08-12 21:25:39 +0530

    Mongoose Basic Configrution

---
+++ server/db/index.js
@@ -0,0 +1,59 @@
+import mongoose from "mongoose";
+
+// const connectDB = async () => {
+//     try {
+//         await mongoose.connect(process.env.MONGODB_URI);
+//         console.log("Database connected")
+//     } catch (error) {
+//         console.log(error);
+//     }    
+// };
+
+// export default connectDB;
+
+
+// import mongoose from "mongoose";
+
+const connectDB = async () => {
+    await mongoose.connect(process.env.MONGODB_URI);
+        console.log("Database connected");
+
+        // Internal flow
+
+            // .env
+            //  ↓
+            // MONGODB_URI
+            //  ↓
+            // process.env.MONGODB_URI
+            //  ↓
+            // mongoose.connect()
+            //  ↓
+            // MongoDB
+
+
+};
+
+ export default connectDB;
+
+
+ //Internally:
+ 
+// App Starts
+//       │
+//       ▼
+// Read .env
+//       │
+//       ▼
+// Get URI
+//       │
+//       ▼
+// Mongoose
+//       │
+//       ▼
+// Open Network Connection
+//       │
+//       ▼
+// Authenticate User
+//       │
+//       ▼
+// Connected ✅
--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,13 @@
 import app from "./app.js";
 import dotenv from "dotenv";
 
+
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 console.log(process.env.JWT_SECRET);
+console.log(process.env.ABC);
+
 
 app.listen(PORT, () =>{
 console.log(`Server is running on port ${PORT}`);
