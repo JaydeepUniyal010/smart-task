77de47c48118896ae15479fb7d3995289d5a3549
JaydeepUniyal010 <jaydeepuniyal07@gmail.com>
2026-08-17 22:51:18 +0530

    Model Update for Schema

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,7 +1,30 @@
-import mongoose from "mongoos";
+import mongoose from "mongoose";
 
-const userSchem = new mongoose.Schema({
-
-});
-
-export default userSchema;
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    phone:{
+        type: String,
+    },
+    password : {
+        type: String,
+        required: true
+    },
+    role:{
+        type: String,
+        enum : ["admin", "user"],
+        default: "user"
+    }, 
+},{
+        timestamps: true,
+    });
+    
+const User = mongoose.model("User", userSchema);
+export default User;
