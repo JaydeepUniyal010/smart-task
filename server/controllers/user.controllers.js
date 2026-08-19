3ebb9366793e9150c32dbe7a1df34f1af2ef3fd0
JaydeepUniyal010 <jaydeepuniyal07@gmail.com>
2026-08-19 23:17:46 +0530

    Remove some unwanted code

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -1,3 +1,5 @@
+import User from "../models/user.model.js";
+
 export const loginUser = (req,res) =>{
     res.send("Login from Controller");
 };
@@ -5,7 +7,12 @@ export const loginUser = (req,res) =>{
 
 
 
-
+// await User.create({
+//     name,
+//     email,
+//     phone,
+//     password
+// });
 
 // Controller ka kaam:
 
